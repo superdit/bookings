92eb1a46db6168e16aab563e2c58f8d0e3a1271b
aditia <superditif@gmail.com>
2022-09-26 13:12:08 +0700

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/superdit/bookings/pkg/config"
+	"github.com/superdit/bookings/pkg/handlers"
+	"github.com/superdit/bookings/pkg/render"
+)
+
+const portNumber = ":8081"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main function
+func main() {
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatalf("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+
+	fmt.Printf("%s", fmt.Sprintf("Staring application on port %s\n", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
